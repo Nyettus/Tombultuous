b4dfbd8409e4fe82ed78b9c9376690bdaf583e4a
Nyettus <112868686+Nyettus@users.noreply.github.com>
2023-08-26 11:49:51 +1000

    - Revert just the room offset

--- Assets/Scripts/Procedural/RoomGridder.cs
+++ Assets/Scripts/Procedural/RoomGridder.cs
@@ -223,40 +223,35 @@ public class RoomGridder : MonoBehaviour
         return -1;
     }
 
-    private int SetRectangular(RoomGrid checkRoom, Vector2Int dimentions, RoomGrid.Shape shape, RoomGrid.State multiGridState,RoomGrid.State startState = RoomGrid.State.occupied,RoomGrid.Type type = RoomGrid.Type.Standard , int offset = 0)
+    private int SetRectangular(RoomGrid checkRoom, Vector2Int dimentions, RoomGrid.Shape shape, RoomGrid.State multiGridState,RoomGrid.State startState = RoomGrid.State.occupied,RoomGrid.Type type = RoomGrid.Type.Standard)
     {
         Vector2Int additiveDimention = dimentions - new Vector2Int(1, 1);
 
         //Quadrants like in math, Z up X right
         bool[] quadrants = { true, true, true, true };
         var furthestPoints = CartesianBounds(checkRoom.position, additiveDimention);
-        Vector2Int[] newStart = new Vector2Int[4];
         List<int> trueIndecies = new List<int>();
         for (int i = 0; i < quadrants.Length; i++)
         {
             //Create offset variant
-            newStart[i] = checkRoom.position + (new Vector2Int(cartesian[i].y, cartesian[i].x)*-offset);
-            quadrants[i] = CheckGrid(newStart[i], furthestPoints[i]);
+            quadrants[i] = CheckGrid(checkRoom.position, furthestPoints[i]);
             if (quadrants[i]) trueIndecies.Add(i);
         }
 
         if (trueIndecies.Count == 0)
         {
-            Debug.Log("" + dimentions + " couldn't place at" + newStart[0]);
+            Debug.Log("" + dimentions + " couldn't place at" + checkRoom.position);
             return 0;
         }
         int randomIndex = trueIndecies[Random.Range(0, trueIndecies.Count)];
         checkRoom.cartesianPlane = randomIndex;
         checkRoom.roomID = multiGridID;
-        ReservePoints(newStart[randomIndex], furthestPoints[randomIndex], shape, multiGridState,startState);
-        RoomGrid finalLocation = activeGrid.Find(location => location.position == newStart[randomIndex]);
+        ReservePoints(checkRoom.position, furthestPoints[randomIndex], shape, multiGridState,startState);
+        RoomGrid finalLocation = activeGrid.Find(location => location.position == checkRoom.position);
         finalLocation.state = startState;
         finalLocation.type = type;
         multiGridID++;
-        if(offset != 0)
-        {
-            Debug.Log("Boss room with offset spawned at: " + newStart[randomIndex]);
-        }
+
         return -1;
 
     }
@@ -362,7 +357,7 @@ public class RoomGridder : MonoBehaviour
                 distance = current;
             }
         }
-        SetRectangular(furthestAdjacents, new Vector2Int(3, 3), RoomGrid.Shape._3x3, RoomGrid.State.forbidden,RoomGrid.State.forbidden,RoomGrid.Type.Boss,1);
+        SetRectangular(furthestAdjacents, new Vector2Int(3, 3), RoomGrid.Shape._3x3, RoomGrid.State.forbidden,RoomGrid.State.forbidden,RoomGrid.Type.Boss);
         furthestAdjacents.state = RoomGrid.State.multiGrid;
         Debug.Log("Boss room created at " + furthestAdjacents.position);
     }
