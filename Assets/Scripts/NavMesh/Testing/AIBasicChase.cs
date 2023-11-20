f0630d87b3bfd23fe887feb1b1d9ba14c5a12544
Nyettus <112868686+Nyettus@users.noreply.github.com>
2023-11-20 15:37:46 +1000

    Update AIBasicChase.cs

--- Assets/Scripts/NavMesh/Testing/AIBasicChase.cs
+++ Assets/Scripts/NavMesh/Testing/AIBasicChase.cs
@@ -7,7 +7,6 @@ public class AIBasicChase : MonoBehaviour
 {
 
     private NavMeshAgent agent;
-    public AnimationCurve m_Curve;
     // Start is called before the first frame update
     void Start()
     {
