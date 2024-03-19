b1c81fc48f33fba27db6f84b29b6c48c8a64fbe2
Nyettus <112868686+Nyettus@users.noreply.github.com>
2024-03-19 17:07:07 +1000

    - More props
     - Terrible candle
     -Scrolls

---
+++ Assets/Scripts/Aesthetic/BadBillboard.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadBillboard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + Camera.main.transform.forward,Vector3.up);
+        
+    }
+}
