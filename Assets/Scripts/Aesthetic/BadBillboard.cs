1d628bfc60eb7d956c9e6e4d9cdc147efbee0690
Nyettus <112868686+Nyettus@users.noreply.github.com>
2024-03-20 15:53:35 +1000

    - Decor into ts1_1x1_1

--- Assets/Scripts/Aesthetic/BadBillboard.cs
+++ Assets/Scripts/Aesthetic/BadBillboard.cs
@@ -1,16 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BadBillboard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void LateUpdate()
     {
         transform.LookAt(transform.position + Camera.main.transform.forward,Vector3.up);
