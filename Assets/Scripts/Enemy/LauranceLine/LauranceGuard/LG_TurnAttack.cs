ab60cb6aa34a1204e553d3220c398ac8f007e9ec
Nyettus <112868686+Nyettus@users.noreply.github.com>
2024-07-09 18:47:35 +1000

    - Nerfed standing behind LG
     - changed phase 2 % to 65%
     - Slightly turns toward the player before a basic attack

---
+++ Assets/Scripts/Enemy/LauranceLine/LauranceGuard/LG_TurnAttack.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UsefulBox;
+
+
+public class LG_TurnAttack : EnemyRootMotion
+{
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+        Vector3 target = MurderBag.RoughPredictLocation(
+            GameManager._.Master.transform.position,
+            GameManager._.Master.movementMaster.rb.velocity,
+            animator.transform.position,
+            5f);
+        FaceTarget(target, 30f);
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateUpdate(animator, stateInfo, layerIndex);
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateExit(animator, stateInfo, layerIndex);
+    }
+}
